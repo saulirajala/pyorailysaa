8033a349083b1f771d285bc999f51109ed505a65
Sauli Rajala <sauli.rajala@valu.fi>
2018-05-06 13:18:42 +0300

    Make splash screen to work

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import {
+	Redirect,
+} from 'react-router-dom';
 
 class SplashScreen extends Component {
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			place: '',
+			redirect: false
+		};
+		// This binding is necessary to make `this` work in the callback
+		this.renderRedirect = this.renderRedirect.bind( this );
+		this.onChange = this.onChange.bind( this );
+		this.handleClick = this.handleClick.bind( this );
+	}
+
+	handleClick() {
+		this.setState({
+			redirect: true
+		});
+	}
+
+	onChange( e ) {
+		this.setState( {
+			place: e.target.value
+		} );
+	}
+
+	renderRedirect( e ) {
+		if (this.state.redirect) {
+			return ( <Redirect to={this.state.place} /> );
+		}
+	}
+
 	render() {
 		return (
 			<div>
+				{this.renderRedirect()}
 				<h2>Syötä paikkakunta</h2>
-				<input type="text"/>
-				<button>Katso sää</button>
+				<input type="text" onChange={this.onChange}/>
+				<button onClick={this.handleClick}>Katso sää</button>
 			</div>
 		);
 	}
